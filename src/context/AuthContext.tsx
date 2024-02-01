da82599139e1a7cd63a5ba485fd94dba79189e67
JhnRavelo <johnravelo135@gmail.com>
2024-02-01 16:58:55 +0300

    create AuthContext

---
+++ src/context/AuthContext.tsx
@@ -0,0 +1,33 @@
+import React, { createContext, useState } from "react";
+
+type AuthUser = {
+    role: number | string
+    accessToken: string 
+    name: string | undefined
+    email: string | undefined
+}
+
+type AuthProviderProps = {
+    children: React.ReactNode
+}
+
+type AuthContextValue = {
+    auth: null | AuthUser
+    setAuth: React.Dispatch<React.SetStateAction<AuthUser | null>>
+}
+
+const AuthContext = createContext<AuthContextValue | null>(null)
+
+const AuthProvider = ({children}:AuthProviderProps)=>{
+    const [auth, setAuth] = useState<null | AuthUser>(null)
+    return (
+        <AuthContext.Provider value={{auth, setAuth}}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
+
+export default AuthContext
+
+export {AuthProvider}
+export type {AuthUser}
