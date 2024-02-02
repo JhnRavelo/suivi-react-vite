4a27c643096665db24e5a55f4be17358d8308c2c
JhnRavelo <johnravelo135@gmail.com>
2024-02-02 14:19:17 +0300

    add utils validationSchema

---
+++ src/utils/validationSchemas.tsx
@@ -0,0 +1,14 @@
+import * as Yup from "yup"
+
+const validateLogin = Yup.object({
+    email: Yup.string().required("Vous devez mettre votre addresse email").email("L'addresse email est invalide"),
+    password: Yup.string().min(8, "Le mot de passe doit avoir au moins 8 caractères")
+        .matches(
+            /[A-Z]/,
+            "Le mot de passe doit contenir au moins une lettre majuscule"
+        )
+        .matches(/[0-9]/, "Le mot de passe doit contenir au moins un chiffre")
+        .required("Le mot de passe est requis")
+})
+
+export { validateLogin }
