bf34954ef40366560ccd81037583c883dd794ded
JhnRavelo <johnravelo135@gmail.com>
2024-02-08 13:46:22 +0300

    update useRefresh hooks

--- src/hooks/useRefresh.tsx
+++ src/hooks/useRefresh.tsx
@@ -1,18 +1,17 @@
 import { defaultAxios } from "../api/axios";
 import useAuth from "./useAuth";
-import { AuthUser } from "../context/AuthContext";
 
 const useRefresh = () => {
     const authContext = useAuth()
     const refresh = async () => {
         try {
             const response = await defaultAxios.get("/refresh");
-            console.log("RES", response.data)
-            authContext?.setAuth((prev): AuthUser => {
-                return {
-                    ...prev, role: response.data.role, accessToken: response.data.accessToken, name: prev?.name, email: prev?.email
-                }
-            })
+            authContext?.setAuth(
+                {role: response.data.role, 
+                accessToken: response.data.accessToken, 
+                name: response.data.name, 
+                email: response.data.email}
+            )
             return response.data
         }
         catch (error) {
