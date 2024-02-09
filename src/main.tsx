1b59dd115388ed9e2e36558e3a9d296f4c274b40
JhnRavelo <johnravelo135@gmail.com>
2024-02-09 16:51:41 +0300

    add Provider to main

--- src/main.tsx
+++ src/main.tsx
@@ -5,13 +5,16 @@ import { BrowserRouter } from 'react-router-dom'
 import { AuthProvider } from './context/AuthContext.tsx'
 import { HeaderProvider } from './context/HeaderContext.tsx'
 import { ProductTypeProvider } from './context/ProductTypeContext.tsx'
+import { UserProvider } from './context/UserContext.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <BrowserRouter>
         <HeaderProvider>
             <AuthProvider>
                 <ProductTypeProvider>
-                    <App />
+                    <UserProvider>
+                        <App />
+                    </UserProvider>
                 </ProductTypeProvider>
             </AuthProvider>
         </HeaderProvider>
