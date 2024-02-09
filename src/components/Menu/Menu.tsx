d4cc609ce6c4eeba30effcc5ea676610fa03211c
JhnRavelo <johnravelo135@gmail.com>
2024-02-09 16:44:28 +0300

    update menu

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -3,22 +3,27 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { Link } from "react-router-dom"
 import "./menu.scss"
 import faProductType from "../../assets/svg/charpenterie.svg"
+import faUser from "../../assets/svg/profile.svg"
 
 const Menu = () => {
   return (
     <div className="menu">
-        <div className="item">
-            <span className="title">MAIN</span>
-            <Link to="/admin" className="listItem">
-                <FontAwesomeIcon icon={faHome} className="icon" />
-                <span className="listItemTitle">Home</span>
-            </Link>
-            <span className="title">LISTES</span>
-            <Link to="/admin/type" className="listItem">
-                <img src={faProductType} alt=""/>
-                <span className="listItemTitle">Type de produits</span>
-            </Link>
-        </div>
+      <div className="item">
+        <span className="title">MAIN</span>
+        <Link to="/admin" className="listItem">
+          <FontAwesomeIcon icon={faHome} className="icon" />
+          <span className="listItemTitle">Home</span>
+        </Link>
+        <span className="title">GESTION</span>
+        <Link to="/admin/type" className="listItem">
+          <img src={faProductType} alt="" />
+          <span className="listItemTitle">Type de produits</span>
+        </Link>
+        <Link to="/admin/user" className="listItem">
+          <img src={faUser} alt="" style={{ width: "15px" }} />
+          <span className="listItemTitle">Utilisateurs</span>
+        </Link>
+      </div>
     </div>
   )
 }
