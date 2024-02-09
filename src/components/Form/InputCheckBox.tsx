3ab684edcc0255da886ee2640665b06bf9b19fdd
JhnRavelo <johnravelo135@gmail.com>
2024-02-09 16:51:06 +0300

    create InputCheckBox input

---
+++ src/components/Form/InputCheckBox.tsx
@@ -0,0 +1,57 @@
+import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { ErrorMessage, Field } from "formik";
+import { useRef } from "react";
+
+
+
+const InputCHeckBox = ({ type, arrays, title, name }) => {
+  const btnListRef = useRef< HTMLDivElement | null >(null);
+  const typeRef = useRef<HTMLUListElement | null>(null);
+
+  const handleListClick = () => {
+    const btnList = btnListRef.current;
+    const typeList = typeRef.current;
+    btnList?.classList.toggle("open");
+    typeList?.classList.toggle("open");
+  };
+  return (
+    <div className="item">
+      <div className="menu-deroulant">
+        <label>{title}</label>
+
+        <div className="container">
+          <div
+            className="select-btn"
+            ref={btnListRef}
+            onClick={handleListClick}
+          >
+            <span className="btn-text">{type && type}</span>
+            <span className="arrow-dwn">
+              <FontAwesomeIcon
+                icon={faChevronDown}
+                className="fa-solid fa-chevron-down"
+              />
+            </span>
+          </div>
+          <ul className="list-type" ref={typeRef}>
+            {arrays && arrays.map((array, index) => (
+              <label className="type" key={index}>
+                <Field
+                  className="checkbox"
+                  type="checkbox"
+                  name={name}
+                  value={array}
+                />
+                <span className="item-text">{array}</span>
+              </label>
+            ))}
+          </ul>
+          <ErrorMessage name={name} component={"p"} className="error" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default InputCHeckBox;
