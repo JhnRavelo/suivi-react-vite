3471099d06b22e93bb5f6faca9079125f4ad043b
JhnRavelo <johnravelo135@gmail.com>
2024-02-09 16:46:43 +0300

    update AdminRouter

--- src/routers/AdminRouter.tsx
+++ src/routers/AdminRouter.tsx
@@ -1,12 +1,16 @@
 import { Route, Routes } from 'react-router-dom'
 import Home from '../pages/Admin/Home/Home'
 import ProductTypes from '../pages/Admin/ProductTypes/ProductTypes'
+import ProductType from '../pages/Admin/ProductType/ProductType'
+import Users from '../pages/Admin/Users/Users'
 
 const AdminRouter = () => {
   return (
     <Routes>
         <Route element={<Home/>} path='/'/>
         <Route element={<ProductTypes/>} path='/type'/>
+        <Route element={<ProductType/>} path='/type/:id' />
+        <Route element={<Users />} path='/user' />
     </Routes>
   )
 }
