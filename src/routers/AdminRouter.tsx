8ed38e8ed87acb718d9d334749849dc3e21d87eb
JhnRavelo <johnravelo135@gmail.com>
2024-02-13 14:29:59 +0300

    add route product to AdminRouter

--- src/routers/AdminRouter.tsx
+++ src/routers/AdminRouter.tsx
@@ -3,14 +3,16 @@ import Home from '../pages/Admin/Home/Home'
 import ProductTypes from '../pages/Admin/ProductTypes/ProductTypes'
 import ProductType from '../pages/Admin/ProductType/ProductType'
 import Users from '../pages/Admin/Users/Users'
+import Products from '../pages/Admin/Products/Products'
 
 const AdminRouter = () => {
   return (
     <Routes>
-        <Route element={<Home/>} path='/'/>
-        <Route element={<ProductTypes/>} path='/type'/>
-        <Route element={<ProductType/>} path='/type/:id' />
-        <Route element={<Users />} path='/user' />
+      <Route element={<Home />} path='/' />
+      <Route element={<ProductTypes />} path='/type' />
+      <Route element={<ProductType />} path='/type/:id' />
+      <Route element={<Users />} path='/user' />
+      <Route element={<Products />} path='/product' />
     </Routes>
   )
 }
