0357d63c210729bf71e0f0abe25f4aede41c2d2d
JhnRavelo <johnravelo135@gmail.com>
2024-02-13 14:20:06 +0300

    update inputFile component

--- src/components/Form/InputFile.tsx
+++ src/components/Form/InputFile.tsx
@@ -1,35 +1,36 @@
 import { ErrorMessage, FormikErrors } from "formik";
 import fileImg from "../../assets/svg/fichier-pdf.svg"
 import { InitialValuesType } from "../../pages/Admin/ProductTypes/ProductTypes";
+import { InitialValues } from "./Form";
 
 type InputFileProps = {
-    name: "pdf",
-    setFieldValue : (field: string, value: File, shouldValidate?: boolean | undefined) => Promise<void | FormikErrors<InitialValuesType>>
-    value: InitialValuesType
+  name: "pdf",
+  setFieldValue: (field: string, value: InitialValues, shouldValidate?: boolean | undefined) => Promise<void | FormikErrors<InitialValues>>
+  value: InitialValuesType | undefined
 }
 
-const InputFile = ({name, setFieldValue, value}: InputFileProps) => {
+const InputFile = ({ name, setFieldValue, value }: InputFileProps) => {
   return (
     <>
-    <input
-      style={{ display: "none" }}
-      id={name}
-      type="file"
-      name={name}
-    //   accept={accept}
-      onChange={(e) => {
-        if (e.target.files) {
+      <input
+        style={{ display: "none" }}
+        id={name}
+        type="file"
+        name={name}
+        //   accept={accept}
+        onChange={(e) => {
+          if (e.target.files) {
             const file = e.target.files[0]
-          setFieldValue(name, file);
-        }
-      }}
-    />
-    <label htmlFor={name} className="labelInput">
-      <img src={fileImg} alt="" style={{width: "20px", height: "20px"}} />
-      <span style={value[name] ? {fontSize:"9px"} : undefined}>{value[name] ? value[name]?.name : "Ajouter le fichier PDF"}</span>
-    </label>
-    <ErrorMessage component={"p"} className="error" name={name}/>
-  </>
+            setFieldValue(name, file);
+          }
+        }}
+      />
+      <label htmlFor={name} className="labelInput">
+        <img src={fileImg} alt="" style={{ width: "20px", height: "20px" }} />
+        <span style={(value && value[name]) ? { fontSize: "9px" } : undefined}>{(value && value[name]) ? value[name]?.name : "Ajouter le fichier PDF"}</span>
+      </label>
+      <ErrorMessage component={"p"} className="error" name={name} />
+    </>
   )
 }
 
