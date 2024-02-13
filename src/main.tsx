b0a45965cffaf90c6974fc463266981ed00a50b2
JhnRavelo <johnravelo135@gmail.com>
2024-02-13 14:26:11 +0300

    add ProductProvider to main

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import { AuthProvider } from './context/AuthContext.tsx'
 import { HeaderProvider } from './context/HeaderContext.tsx'
 import { ProductTypeProvider } from './context/ProductTypeContext.tsx'
 import { UserProvider } from './context/UserContext.tsx'
+import { ProductProvider } from './context/ProductContext.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <BrowserRouter>
@@ -13,7 +14,9 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
             <AuthProvider>
                 <ProductTypeProvider>
                     <UserProvider>
-                        <App />
+                        <ProductProvider>
+                            <App />
+                        </ProductProvider>
                     </UserProvider>
                 </ProductTypeProvider>
             </AuthProvider>
