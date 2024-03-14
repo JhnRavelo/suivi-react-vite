6433593e364745978c842652e0798f6bfb674d9a
JhnRavelo <johnravelo135@gmail.com>
2024-03-14 17:00:57 +0300

    add top and bigdata to home pages

--- src/pages/Admin/Home/Home.tsx
+++ src/pages/Admin/Home/Home.tsx
@@ -1,9 +1,44 @@
+import { useEffect, useState } from "react"
+import TopProduct from "../../../components/TopProduct/TopProduct"
+import useHome from "../../../hooks/useHome"
+import useHeader from "../../../hooks/useHeader"
+import { StatProblems } from "../../../context/HomeContext"
+import BigDataChart from "../../../components/BigDataChart/BigDataChart"
+import getChartPerMonth from "../../../utils/getChartPerMonth"
+import { BigCharDatas } from "../../../assets/ts/data"
 
 const Home = () => {
+  const homeContext = useHome()
+  const headerContext = useHeader()
+  const [topProblem, setTopProblem] = useState<StatProblems>()
+  const [bigChartData, setBigChartData] = useState<BigCharDatas>()
+
+  useEffect(() => {
+      if (homeContext?.statTop && headerContext?.year) {
+          const filterStatTopPerYear = homeContext.statTop.filter(item => item.year == headerContext?.year)
+          setTopProblem(filterStatTopPerYear)
+      }
+  }, [homeContext?.statTop, headerContext?.year])
+
+  useEffect(()=>{
+    if(homeContext?.statSuivis && headerContext?.year){
+      const filterStatSuivisPerYear = homeContext.statSuivis.filter(item => item.year == headerContext.year)
+      const statSuivis = getChartPerMonth(filterStatSuivisPerYear)
+      setBigChartData(statSuivis)
+    }
+  }, [homeContext?.statSuivis, headerContext?.year])
 
   return (
-    <div>
-      Home
+    <div className="home">
+      <div className="box box1">
+        <TopProduct topProblem={topProblem}/>
+      </div>
+      <div className="box box7">
+        <BigDataChart chartData={bigChartData}/>
+      </div>
+      <div className="box box4">
+        {/* <PieChartBox /> */}
+      </div>
     </div>
   )
 }
