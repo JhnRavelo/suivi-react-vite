c6aa9f562b9a39c07ce9a949b20da510b73557c0
JhnRavelo <johnravelo135@gmail.com>
2024-03-14 16:55:55 +0300

    create bigDataChart component

---
+++ src/components/BigDataChart/BigDataChart.tsx
@@ -0,0 +1,61 @@
+import { BigCharDatas } from '../../assets/ts/data';
+import './bigDataChart.scss'
+import {
+    Bar,
+    BarChart,
+    ResponsiveContainer,
+    Tooltip,
+    XAxis,
+} from "recharts";
+
+type BigChartDataProps = {
+    chartData: BigCharDatas | undefined
+}
+
+type Payload = {
+    datakey: string
+    payload: { name: string }
+    value: number
+}
+
+const CustomTooltip: React.FC<{ active?: boolean; payload?: Payload[] }> = ({ active, payload }) => {
+    if (active && payload && payload.length) {
+        return (
+            <div className="custom-tooltipBig">
+                {payload.map((item, index) => {
+                    return (
+                        <p key={index} className={`data ${item.datakey}`} >
+                            {`${item.payload.name}: ${item.value} Suivis`}
+                        </p>
+                    );
+                })}
+            </div>
+        );
+    } else {
+        return null;
+    }
+};
+
+const BigDataChart = ({ chartData }: BigChartDataProps) => {
+    return (
+        <div className="bigChartBox">
+            <h1>Total Suivis</h1>
+            <div className="chart">
+                <ResponsiveContainer width="99%" height="100%">
+                    <BarChart data={chartData}>
+                        <Tooltip
+                            contentStyle={{ background: "#2a3447", borderRadius: "5px" }}
+                            labelStyle={{ display: "none" }}
+                            cursor={{ fill: "none" }}
+                            content={<CustomTooltip />}
+                        />
+                        <XAxis dataKey="name" />
+                        <Bar dataKey="suivis" fill="#FF8042" />
+                    </BarChart>
+                </ResponsiveContainer>
+            </div>
+        </div>
+    );
+};
+
+export default BigDataChart;
