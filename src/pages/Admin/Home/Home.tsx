81121acf21b823ad2451a4defc525ad707eee334
JhnRavelo <johnravelo135@gmail.com>
2024-03-18 09:23:38 +0300

    add PieChart component to Home page

--- src/pages/Admin/Home/Home.tsx
+++ src/pages/Admin/Home/Home.tsx
@@ -2,16 +2,19 @@ import { useEffect, useState } from "react"
 import TopProduct from "../../../components/TopProduct/TopProduct"
 import useHome from "../../../hooks/useHome"
 import useHeader from "../../../hooks/useHeader"
-import { StatProblems } from "../../../context/HomeContext"
+import { StatTops } from "../../../context/HomeContext"
 import BigDataChart from "../../../components/BigDataChart/BigDataChart"
 import getChartPerMonth from "../../../utils/getChartPerMonth"
 import { BigCharDatas } from "../../../assets/ts/data"
+import ChartPie, { PieCharData } from "../../../components/PieChart/ChartPie"
+import getPieChart from "../../../utils/getPieChart"
 
 const Home = () => {
   const homeContext = useHome()
   const headerContext = useHeader()
-  const [topProblem, setTopProblem] = useState<StatProblems>()
+  const [topProblem, setTopProblem] = useState<StatTops>()
   const [bigChartData, setBigChartData] = useState<BigCharDatas>()
+  const [pieChartData, setPieChartData] = useState<PieCharData>()
 
   useEffect(() => {
       if (homeContext?.statTop && headerContext?.year) {
@@ -28,6 +31,14 @@ const Home = () => {
     }
   }, [homeContext?.statSuivis, headerContext?.year])
 
+  useEffect(()=>{
+    if(homeContext?.statProductTypes && headerContext?.year){
+      const filterStatProductTypesPerYear = homeContext.statProductTypes.filter(item => item.year == headerContext.year)
+      const statProductTypes = getPieChart(filterStatProductTypesPerYear)
+      setPieChartData(statProductTypes)
+    }
+  },[homeContext?.statProductTypes, headerContext?.year])
+
   return (
     <div className="home">
       <div className="box box1">
@@ -37,7 +48,7 @@ const Home = () => {
         <BigDataChart chartData={bigChartData}/>
       </div>
       <div className="box box4">
-        {/* <PieChartBox /> */}
+        <ChartPie chartData={pieChartData} />
       </div>
     </div>
   )
