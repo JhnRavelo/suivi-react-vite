9372b878fae1aa51f3c3aa462747688109501870
JhnRavelo <johnravelo135@gmail.com>
2024-03-22 08:46:02 +0300

    add isImport to headerContext

--- src/context/HeaderContext.tsx
+++ src/context/HeaderContext.tsx
@@ -2,29 +2,43 @@ import { createContext, useState } from "react";
 import { ProviderProps } from "./AuthContext";
 
 type HeaderContextValue = {
-    years: string[]
-    setYears: React.Dispatch<React.SetStateAction<string[]>>
-    year: NbrStrUn
-    setYear: React.Dispatch<React.SetStateAction<NbrStrUn>>
-    notifs: string[]
-    setNotifs: React.Dispatch<React.SetStateAction<string[]>>
-}
+  years: string[];
+  setYears: React.Dispatch<React.SetStateAction<string[]>>;
+  year: NbrStrUn;
+  setYear: React.Dispatch<React.SetStateAction<NbrStrUn>>;
+  notifs: string[];
+  setNotifs: React.Dispatch<React.SetStateAction<string[]>>;
+  isImport: boolean;
+  setIsImport: React.Dispatch<React.SetStateAction<boolean>>;
+};
 
-export type NbrStrUn = number | string | undefined
+export type NbrStrUn = number | string | undefined;
 
-const HeaderContext = createContext<null | HeaderContextValue>(null)
+const HeaderContext = createContext<null | HeaderContextValue>(null);
 
 const HeaderProvider = ({ children }: ProviderProps) => {
-    const [years, setYears] = useState<Array<string>>([])
-    const [year, setYear] = useState<NbrStrUn>()
-    const [notifs, setNotifs] = useState<string[]>([])
-    return (
-        <HeaderContext.Provider value={{ years, setYear, year, setYears, notifs, setNotifs }}>
-            {children}
-        </HeaderContext.Provider>
-    )
-}
+  const [years, setYears] = useState<Array<string>>([]);
+  const [year, setYear] = useState<NbrStrUn>();
+  const [notifs, setNotifs] = useState<string[]>([]);
+  const [isImport, setIsImport] = useState<boolean>(false);
+  return (
+    <HeaderContext.Provider
+      value={{
+        years,
+        setYear,
+        year,
+        setYears,
+        notifs,
+        setNotifs,
+        isImport,
+        setIsImport,
+      }}
+    >
+      {children}
+    </HeaderContext.Provider>
+  );
+};
 
-export default HeaderContext
+export default HeaderContext;
 
-export { HeaderProvider }
+export { HeaderProvider };
